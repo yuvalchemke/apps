a27c369385ec3fa6bafe862bd9cb6eba7c48f2be
yuvalchemke <109233742+yuvalchemke@users.noreply.github.com>
2022-12-08 19:12:04 +0200

    Update apps_aruco.py

--- apps_aruco.py
+++ apps_aruco.py
@@ -38,5 +38,5 @@ app_mode = st.sidebar.selectbox('Dictionary',
 st.sidebar.markdown('---') # adds a devider (a line)
 
 # choosing a k value (either with +- or with a slider)
-  MarkerID = st.sidebar.number_input('Marker ID (number of clusters):', value=4, min_value = 1) # asks for input from the user
+  a = st.sidebar.number_input('Marker ID (number of clusters):', value=4, min_value = 1) # asks for input from the user
   st.sidebar.markdown('---') # adds a devider (a line)
