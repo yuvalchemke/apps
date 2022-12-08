40ae2f3bb84e4099f860b9bc84ce445e2700308e
yuvalchemke <109233742+yuvalchemke@users.noreply.github.com>
2022-12-08 19:01:23 +0200

    Update apps_aruco.py

--- apps_aruco.py
+++ apps_aruco.py
@@ -1,12 +1,6 @@
 # libaries
 import streamlit as st
-from skimage import measure, io, img_as_ubyte, morphology, util, color
-import matplotlib.pyplot as plt
-from skimage.color import label2rgb, rgb2gray
-import numpy as np
-import pandas as pd
-import cv2
-import imutils
+
 
 # vars
 plant = 'plant.png'
