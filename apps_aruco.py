02c23681d8c6c9f4b9d2ad1f4ebc69d5c413e71b
yuvalchemke <109233742+yuvalchemke@users.noreply.github.com>
2022-12-08 19:05:46 +0200

    Update apps_aruco.py

--- apps_aruco.py
+++ apps_aruco.py
@@ -9,7 +9,40 @@ plant = 'plant.png'
 st.set_page_config(page_title='Plant-Since-AruCo', page_icon = plant, layout = 'wide', initial_sidebar_state = 'auto')
 st.title('yuval chemke web-app')
 
+
+# vars
+plant = 'plant.png'
+
+# main page
+st.set_page_config(page_title='Plant-Since-AruCo', page_icon = plant, layout = 'wide', initial_sidebar_state = 'auto')
+st.title('yuval chemke web-app')
+
 # side bar
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] . div:first-child{
+        width: 350px
+    }
+    
+    [data-testid="stSidebar"][aria-expanded="false"] . div:first-child{
+        width: 350px
+        margin-left: -350px
+    }    
+    </style>
+    
+    """,
+    unsafe_allow_html=True,
+
+
+)
+
+st.sidebar.title('Work Flow')
+st.sidebar.subheader('Upload an image with an AruCu on it /n https://chev.me/arucogen/')
+
+# add dropdown to select pages on left
+app_mode = st.sidebar.selectbox('Dictionary',
+                                  ['4X4', '5X5', '6X6', '7X7'])
 st.markdown(
     """
     <style>
