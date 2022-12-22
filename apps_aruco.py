8e867e0bcf25328752488baa08fcecf2fd832aa8
yuvalchemke <109233742+yuvalchemke@users.noreply.github.com>
2022-12-22 10:54:23 +0200

    Update apps_aruco.py

--- apps_aruco.py
+++ apps_aruco.py
@@ -16,7 +16,7 @@ img_file_buffer = st.sidebar.file_uploader("Upload an image", type=['jpg', 'jpeg
 if img_file_buffer is not None:
     image = io.imread(img_file_buffer)
 else: # if no image was uploaded, then segment the demo image
-    demo_image = tomato.jpg
+    demo_image = 'tomato.jpg'
     image = io.imread(demo_image)
 
  # Display the result on the right (main frame)
